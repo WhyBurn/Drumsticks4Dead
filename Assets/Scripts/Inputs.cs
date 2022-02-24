cb3e6a3ab9b5d906c917231306919345ca9ac9b4
Flameingblast <56851336+WhyBurn@users.noreply.github.com>
2022-02-24 15:56:43 -0800

    BetterInputAndAbstractController
    Made a character controller class that has the movement, which player controller inherits. Player controller takes input from the static Inputs class which is assigned by the InputController class. This allows for ease of adding more player characters.

---
+++ Assets/Scripts/Inputs.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Inputs
+{
+    public static Vector2[] playerMovements;
+    public static bool[] playerInteractDown;
+    public static bool[] playerInteractHold;
+    public static bool[] playerInteractUp;
+    public static bool[] playerThrowDown;
+    public static bool[] playerThrowHold;
+    public static bool[] playerThrowUp;
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,47 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(Rigidbody2D))]
-public class PlayerController : MonoBehaviour
+
+public class PlayerController : CharacterController
 {
-    public float maxSpeed;
-    public float maxAcceleration;
+    public int playerNumber;
 
-    private Rigidbody2D body;
-
-    // Start is called before the first frame update
-    void Start()
+    override public Vector2 GetMovement()
     {
-        body = gameObject.GetComponent<Rigidbody2D>();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        Vector2 playerInput = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
-        if(playerInput.magnitude > 1)
-        {
-            playerInput.Normalize();
-        }
-        Vector2 targetVelocity = playerInput * maxSpeed;
-        float horizontalDifference = targetVelocity.x - body.velocity.x;
-        float vertiaclDifference = targetVelocity.y - body.velocity.y;
-        if(horizontalDifference > 0)
-        {
-            horizontalDifference = Mathf.Min(horizontalDifference, maxAcceleration);
-        }
-        else
-        {
-            horizontalDifference = Mathf.Max(horizontalDifference, maxAcceleration * -1);
-        }
-        if (vertiaclDifference > 0)
-        {
-            vertiaclDifference = Mathf.Min(vertiaclDifference, maxAcceleration);
-        }
-        else
-        {
-            vertiaclDifference = Mathf.Max(vertiaclDifference, maxAcceleration * -1);
-        }
-        body.AddForce(new Vector2(horizontalDifference, vertiaclDifference), ForceMode2D.Force);
+        return (Inputs.playerMovements[playerNumber]);
     }
 }
