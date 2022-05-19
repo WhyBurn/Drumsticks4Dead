f3d890a5de4971b6c2683b2299bf7dae96df0659
Archie <archi.faug@gmail.com>
2022-05-19 15:34:52 -0700

    ImprovedControl
    Made it so the player rotates towards the direction they're pressing better, and increased interact distance. However, this makes it harder to aim when not using a controller, as it is nearly impossible to aim in a direction other than one of the 8 cardinal directions.

--- Assets/Scripts/CharacterAnimator.cs
+++ Assets/Scripts/CharacterAnimator.cs
@@ -2,28 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(CharacterController))]
 public class CharacterAnimator : MonoBehaviour
 {
-    private Rigidbody2D body;
+    private CharacterController body;
     // Start is called before the first frame update
     void Start()
     {
-        body = gameObject.GetComponent<Rigidbody2D>();
+        body = gameObject.GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (body.velocity.magnitude > 0.1f)
+        if (body.GetMovement().magnitude > 0.1f)
         {
-            Vector2 direction = body.velocity.normalized;
+            Vector2 direction = body.GetMovement().normalized;
             float angle = Mathf.Acos(direction.y) * Mathf.Rad2Deg;
             if(direction.x > 0)
             {
                 angle *= -1;
             }
-            transform.rotation = Quaternion.Euler(0, 0, angle);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(0, 0, angle), Time.deltaTime * 1000f);
         }
     }
 }
